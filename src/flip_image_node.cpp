7234e7ee368ac6ccebcbfb6315bf2efa684970fe
reyarcenegui@gmail.com <reyarcenegui@gmail.com>
2019-12-31 13:52:33 +0100

    added 90º rotation for nix thermal cam

--- src/flip_image_node.cpp
+++ src/flip_image_node.cpp
@@ -10,11 +10,19 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 	try
 	{
 		// Flip input image
-		cv_bridge::CvImage flipImage;               
-		cv::flip(cv_bridge::toCvShare(msg, "")->image, flipImage.image, flip_type);     // -1 means horizontal and vertical flip
+		cv_bridge::CvImage flipImage;    
+		if(flip_type == 2) {
+			cv::transpose(cv_bridge::toCvShare(msg, "")->image,flipImage.image );
+
+			cv::flip(flipImage.image, flipImage.image, flip_type);     // -1 means horizontal and vertical flip
+			cv::flip(flipImage.image, flipImage.image, -1);   
                 flipImage.header = msg->header;
                 flipImage.encoding = msg->encoding;
-		
+		}else{
+			cv::flip(cv_bridge::toCvShare(msg, "")->image, flipImage.image, flip_type);     // -1 means horizontal and vertical flip
+                flipImage.header = msg->header;
+                flipImage.encoding = msg->encoding;
+		}
 		// Publish flip image
         pub.publish(flipImage.toImageMsg());
 	}
@@ -27,8 +35,8 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "flip_image_node");
 	ros::NodeHandle nh;
-        
-        nh.getParam("flip_type",flip_type);
+	ros::NodeHandle lnh("~");
+        lnh.getParam("flip_type",flip_type);
         
 	image_transport::ImageTransport it(nh);
 	image_transport::Subscriber sub = it.subscribe("input_image", 1, imageCallback);
